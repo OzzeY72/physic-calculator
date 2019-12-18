270f3b8993aaaf858df118db6552147e002577dc
unknown <nabacap72@gmail.com>
2019-12-18 18:27:46 +0300

    Style changes

--- containers/main/index.js
+++ containers/main/index.js
@@ -7,21 +7,14 @@ const funcName = (value) => `on${isNaN(parseInt(value)) ? value : 'Tap'}`;
 export const MainPageView = ({ scope, buttons }) => {
   const { highlight, calculation, result} = scope.state;
   return (
-    <Container>
+    <Container style={styles.container}>
       <Grid>
-        <Row style={styles.history.base}>
-          <Col style={styles.history.content.base}>
-            <Button onPress={() => scope.props.navigation.navigate('Formulas')} block>
-              <Text>History</Text>
-            </Button>
-          </Col>
-        </Row>
         <Row style={styles.head.base}>
           <Col style={styles.head.text.base}>
             <Item regular style={{flex: 1}} disabled>
               <Input style={highlight ? styles.shadow_text : styles.base_text} value={calculation || '0'} disabled />
             </Item>
-            <Item regular style={{flex: 1}} disabled>
+            <Item regular style={{flex: 1},{backgroundColor:'#3E3331'}} disabled>
               <Input style={[styles.base_text, !highlight ? styles.shadow_text : styles.base_text]} value={result} disabled />
             </Item>
           </Col>
@@ -32,9 +25,9 @@ export const MainPageView = ({ scope, buttons }) => {
               buttons.map((row, i) => (
                 <Row key={i}>
                   {
-                    row.map(({warning, dark, light, value, title}, index) => (
+                    row.map(({warning, dark, light, value, title}, index) => ( 
                       <Col key={index}>
-                        <Button
+                        <Button transparent
                           onPress={() => scope[funcName(value)](value)}
                           style={styles.button}
                           block
@@ -42,7 +35,7 @@ export const MainPageView = ({ scope, buttons }) => {
                           dark={dark}
                           light={light}
                         >
-                          <Text>{title}</Text>
+                          <Text style={styles.button.text}>{title}</Text>
                         </Button>
                       </Col>
                     ))
--- containers/main/styles.js
+++ containers/main/styles.js
@@ -1,31 +1,40 @@
 export const styles = {
+  container: {
+   backgroundColor:'#3E3331' 
+  },
   button: {
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor:'#3E3331',
     flex: 1,
-    margin: 1
+    margin: 2,
+      text: {
+        fontSize: 25
+      }
   },
   shadow_text: {
     textAlign: "right",
     color: "#9c9c9c",
-    fontSize: 20
+    fontSize: 30
   },
   base_text: {
     textAlign: "right",
-    fontSize: 30
+    fontSize: 50,
+    color:'#FBF8F8'
   },
-  history: {
+  /*history: {
     base: {
       flexDirection: "row",
-      backgroundColor: "#DCD0C0"
+      backgroundColor: "#241D1C"
     },
     content: {
       base: {},
     }
-  },
+  },*/
   head: {
     base: {
-      flex: 4
+      flex: 4,
+      backgroundColor:'#3E3331',
     },
     text: {
       base: {
